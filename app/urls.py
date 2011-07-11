2bed3dc8717cf2d092bae902587e54b397d1ede5
David Volquartz Lebech <david.lebech@gmail.com>
2011-07-11 11:42:20 +0200

    Deleted hello world example

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,6 @@ rules = [
     Rule('/', name='index', redirect_to= '/main'),
     Rule('/<string:universe>/', name='index', handler='tu.site.IndexHandler'),
     Rule('/<string:universe>/login', 'site-login', handler='tu.site.LoginHandler'),
-    Rule('/pretty', name='hello-world-pretty', handler='hello_world.handlers.PrettyHelloWorldHandler'),
     HandlerPrefix('tu.text.', [
         Rule('/<string:universe>/text/all', 'texts-all', handler='AllTextsHandler'),
         Rule('/<string:universe>/text/mine', 'texts-mine', handler='MyTextsHandler'),
